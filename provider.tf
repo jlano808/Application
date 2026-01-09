c84a97eb469a9f8aa134805e273cffe6bb5d065d
Jason <jasonlano@gmail.com>
2026-01-09 14:10:11 -0500

    Standardized project: Added variables, outputs, and gitignore

--- provider.tf
+++ provider.tf
@@ -1,12 +1,14 @@
 terraform {
+  backend "s3" {
+    bucket = "jlano001"
+    key    = "state/ec2.tfstate"
+    region = "us-east-1" # Ensure this matches your bucket's region
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
-}
-
-provider "aws" {
-  region = "us-east-1" # <--- Change this to your EC2's region
 }
